4aded4d80c8d1b0c8c660d5d734fdb16160addb1
brandly3 <brandly3@msu.edu>
2021-05-10 22:48:41 -0400

    removed highlight from distribution graph

--- src/js/components/CalcOutput.js
+++ src/js/components/CalcOutput.js
@@ -4,50 +4,6 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
 import { CalcOutputNumbers } from "./CalcOutputNumbers.js";
 import { CalcOutputText } from "./CalcOutputText.js";
 
-// const data = [{
-//         name: 'Page A',
-//         uv: 4000,
-//         pv: 2400,
-//         amt: 2400,
-//     },
-//     {
-//         name: 'Page B',
-//         uv: 3000,
-//         pv: 1398,
-//         amt: 2210,
-//     },
-//     {
-//         name: 'Page C',
-//         uv: 2000,
-//         pv: 9800,
-//         amt: 2290,
-//     },
-//     {
-//         name: 'Page D',
-//         uv: 2780,
-//         pv: 3908,
-//         amt: 2000,
-//     },
-//     {
-//         name: 'Page E',
-//         uv: 1890,
-//         pv: 4800,
-//         amt: 2181,
-//     },
-//     {
-//         name: 'Page F',
-//         uv: 2390,
-//         pv: 3800,
-//         amt: 2500,
-//     },
-//     {
-//         name: 'Page G',
-//         uv: 3490,
-//         pv: 4300,
-//         amt: 2100,
-//     },
-// ];
-
 const toPercent = (decimal, fixed = 2) => {
     return `${(decimal * 100).toFixed(fixed)}%`;
 };
@@ -86,7 +42,7 @@ export class CalcOutput extends Component {
             <div class='flex-container-vertical' style={{    padding: '1em', border: '1px dashed #666'}}>
                 <CalcOutputNumbers calcs={calcs} ttk={this.props.ttk}/>
                 <CalcOutputText calcs={calcs} />
-                <div class='highlight-section'>
+                <div>
                     <div class="center">
                         Hit Distribution (dmg > 0)
                     </div>
@@ -106,7 +62,7 @@ export class CalcOutput extends Component {
                               <CartesianGrid strokeDasharray="3 3" stroke="#666" />
                               <XAxis dataKey="damage" name="Damage" stroke="#ddd" />
                               <YAxis stroke="#ddd" tickFormatter={toPercent}/>
-                              <Tooltip className="highligh-section"/>
+                              <Tooltip className="highlight-section"/>
                               <Bar dataKey="likelihood" fill="#9eff74" />
                             </BarChart>
                           </ResponsiveContainer>
