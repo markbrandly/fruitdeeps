5e22600494ebf836128cd2cba76c1fec37c2f7e9
brandly <brandly13@gmail.com>
2021-05-23 18:28:25 -0400

    inline initial playerList objects

--- src/js/reducers/reducer.js
+++ src/js/reducers/reducer.js
@@ -3,15 +3,9 @@ import Player from '../lib/Player.js';
 var Newtroost = { "name": "Sotetseg", "image": "./assets/monster_images/Sotetseg.png", "version": "", "combat": 995, "stats": { "amagic": 0, "arange": 0, "att": 250, "attbns": 0, "dcrush": 70, "def": 200, "dmagic": 30, "drange": 150, "dslash": 70, "dstab": 70, "hitpoints": 4000, "mage": 250, "mbns": 0, "range": 0, "rngbns": 0, "str": 250, "strbns": 48 }, "attributes": [] }
 
 const defaultState = {
-    playerList: [],
+    playerList: [{}, {}],
     monster: Newtroost
 }
-
-for (let i = 0; i < 2; i++) {
-    defaultState.playerList.push({})
-}
-
-
 
 function reducer(state = defaultState, action) {
     const newState = { ...state }
