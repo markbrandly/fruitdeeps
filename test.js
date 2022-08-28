f685a93e9d40f08e0c1fd349c92134dd4332cc4c
Mark Brandly <brandly3@msu.edu>
2022-08-27 23:54:24 -0400

    Added toa calcs

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-import pass from './passwords.js'
+const accuracyFormula = (atk, def) => {
+    let clamp = (n) => Math.max(0, Math.min(1, n))
 
-console.log(pass)
+    if (atk > def) {
+        return clamp(1 - (def + 2)*(2*def+3)/(atk+1)/(atk+1)/6)
+    } else {
+        return clamp(atk * (4 * atk + 5) / 6 / (atk + 1) / (def + 1))
+    }
+}
+
+console.log(accuracyFormula(1000,10000))
