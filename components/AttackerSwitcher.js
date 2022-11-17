dfee2d0ecf9b2812478454c97e5066b73acc636b
Jacob Lindelof <jlindelof@me.com>
2022-11-17 14:45:20 -0500

    removed un-used useEffect import

--- components/AttackerSwitcher.js
+++ components/AttackerSwitcher.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Image from "next/image";
 import { Attacker } from './Attacker.js';
